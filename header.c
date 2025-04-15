2478fcd5003bf520b94e9f02dfdfad049d279775
AlexBrinzei <mihai.brinzei@stud.acs.upb.ro>
2025-04-15 17:10:10 +0300

    afisare corecta, punctaj 0(

--- header.c
+++ header.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "header.h"
 
-void read_file(FILE *file, int *n, int *m, int *k, char ***mat)
+
+
+void read_file(FILE *file, int *t, int *n, int *m, int *k, char ***mat)
 {
-    int test_number;
-    fscanf(file, "%d", &test_number);
+    //t = task number
+    fscanf(file, "%d", t);
     fscanf(file, "%d %d", n, m);
     fscanf(file, "%d", k);
 
@@ -88,6 +88,14 @@ void rules(char **mat, int n, int m)
     }
     free(aux);
 }
+void free_matrix(char **mat, int n)
+{
+    for (int i = 0; i < n; i++) {
+        free(mat[i]);
+    }
+    free(mat);
+}
+
 
 void display(char **mat, int n, int m, FILE *output)
 {
@@ -100,10 +108,163 @@ void display(char **mat, int n, int m, FILE *output)
     fprintf(output, "\n");
 }
 
-void free_matrix(char **mat, int n)
-{
-    for (int i = 0; i < n; i++) {
-        free(mat[i]);
-    }
-    free(mat);
+//Functii pentru task 2
+
+
+// Creează un nod nou de celulă
+CellNode* create_cell_node(int row, int col) {
+    CellNode *new_node = (CellNode*) malloc(sizeof(CellNode));
+    new_node->row = row;
+    new_node->col = col;
+    new_node->next = NULL;
+    return new_node;
 }
+
+// Adaugă o celulă la sfârșitul listei
+void add_cell(CellNode **head, int row, int col) {
+    CellNode *new_node = create_cell_node(row, col);
+    if (*head == NULL) {
+        *head = new_node;
+    } else {
+        CellNode *temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = new_node;
+    }
+}
+
+
+//afisare celulele dintr o generatie sub forma (row, col);
+void print_cell_list(CellNode *head) {
+    while(head) {
+        printf("(%d,%d);", head->row, head->col);
+        head = head->next;
+    }
+}
+
+
+//elibereaza memoria unei liste de celule modificate
+void delete_cell_list(CellNode **head) {
+    while(*head) {
+        CellNode *temp = *head;
+        *head = (*head)->next;
+        free(temp);
+    }
+}
+
+
+//verifica daca stiva de generatii este goala
+int isEmpty(Generation *top)
+{
+    return top == NULL;
+}
+
+
+
+void push(Generation **top, CellNode *cellList) {
+    Generation *newGen = (Generation*) malloc(sizeof(Generation));
+    newGen->cells = cellList;
+    newGen->next = *top;
+    *top = newGen;
+}
+
+
+
+
+
+
+CellNode* pop_stack(Generation **top) {
+    if (isEmpty(*top)) 
+        return NULL;
+
+    Generation *temp = *top;
+    CellNode *cells = temp->cells;
+    *top = temp->next;
+    free(temp);
+    return cells;
+}
+
+void delete_stack(Generation **top) {
+    while (!isEmpty(*top)) {
+        CellNode *cells = pop_stack(top);
+        delete_cell_list(&cells);
+    }
+}
+
+Generation* reverse_stack(Generation *top) {
+    Generation *prev = NULL, *current = top, *next = NULL;
+    while (current != NULL) {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+    return prev;
+}
+
+
+void insert_cell_sorted(CellNode **head, int row, int col) {
+    CellNode *new_node = create_cell_node(row, col);
+   
+    
+    // Cazul 1: Lista este goală sau noul nod trebuie inserat înaintea capului
+    if (*head == NULL || (row < (*head)->row) || (row == (*head)->row && col < (*head)->col)) {
+        new_node->next = *head;
+        *head = new_node;
+        return;
+    }
+
+    // Căutăm poziția corectă pentru inserare
+    CellNode *current = *head;
+    while (current->next != NULL && 
+          ((current->next->row < row) || 
+           (current->next->row == row && current->next->col < col))) {
+        current = current->next;
+    }
+
+    // Inserăm noul nod
+    new_node->next = current->next;
+    current->next = new_node;
+}
+
+
+
+
+
+CellNode* gen_differences(char **old_gen, char **new_gen, int n, int m) {
+    CellNode *diffs = NULL;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            
+            
+            if(old_gen[i][j] != new_gen[i][j]) {
+                
+                    insert_cell_sorted(&diffs, i, j); 
+                
+                
+                
+            }
+        }
+    }
+    return diffs;
+}
+
+
+
+//scrie stiva in fisier cu indexul fiecarei generatii 
+void write_stack_to_file(Generation *top, FILE *f) {
+    int gen_id = 1;
+    while (top != NULL) {
+        fprintf(f, "%d ", gen_id++);
+        CellNode *current = top->cells;
+        while (current != NULL) {
+            fprintf(f, "%d %d ", current->row, current->col);
+            current = current->next;
+        }
+        fprintf(f, "\n"); // generație nouă pe linie nouă
+        top = top->next;
+    }
+}
+
+
--- main.c
+++ main.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "header.h"
 
 int main(int argc, const char *argv[])
@@ -7,30 +5,82 @@ int main(int argc, const char *argv[])
 
     FILE *file = fopen(argv[1], "rt");
     if (!file) {
-        printf("Eroare la deschiderea fisierului de intrare!\n");
+        printf("Eroare la deschiderea fisierului de intrare!");
         return 1;
     }
 
     FILE *output = fopen(argv[2], "wt");
     if (!output) {
-        printf("Eroare la deschiderea fisierului de iesire!\n");
+        printf("Eroare la deschiderea fisierului de iesire!");
         fclose(file);
         return 1;
     }
 
-    int n, m, k;
+    int t, n, m, k;
     char **mat;
+    read_file(file, &t, &n, &m, &k, &mat);
 
-    read_file(file, &n, &m, &k, &mat);
 
-    for (int gen = 0; gen <= k; gen++) {
-        display(mat, n, m, output);
-        rules(mat, n, m);
+
+    if(t == 1)
+    {
+        for (int gen = 0; gen <= k; gen++) {
+            display(mat, n, m, output);
+            rules(mat, n, m);
+        }
+    
+        fclose(file);
+        fclose(output);
+        free_matrix(mat, n);
     }
 
-    fclose(file);
-    fclose(output);
-    free_matrix(mat, n);
 
-    return 0;
-}
+
+    if(t == 2)
+    {
+
+    // Declaram stiva care va tine diferentele intre generatii
+     Generation *stack = NULL;
+
+     // Alocam o copie a matricei curente pentru comparatie
+     char **prev_gen =(char**) malloc(n * sizeof(char*));
+     for (int i = 0; i < n; i++) {
+         prev_gen[i] = (char*)malloc(m * sizeof(char));
+         for (int j = 0; j < m; j++)
+             prev_gen[i][j] = mat[i][j];
+     }
+ 
+     for (int gen = 0; gen <= k; gen++) {
+         // Afisam generatia curenta
+
+         
+ 
+         if (gen > 0) {
+             // Comparatie intre generatia anterioara si cea curenta
+             CellNode *diff = gen_differences(prev_gen, mat, n, m);
+             push(&stack, diff);
+         }
+ 
+         // Salvam matricea curenta in prev_gen inainte sa o modificam
+         for (int i = 0; i < n; i++)
+             for (int j = 0; j < m; j++)
+                 prev_gen[i][j] = mat[i][j];
+ 
+         // Aplicam regulile jocului pentru urmatoarea generatie
+         rules(mat, n, m);
+     }
+ 
+     stack = reverse_stack(stack);
+     write_stack_to_file(stack, output);
+     
+ 
+     // Curatam memoria
+     delete_stack(&stack);
+     free_matrix(mat, n);
+     free_matrix(prev_gen, n);
+     fclose(file);
+     fclose(output);
+    }
+ 
+     return 0;
+ }
